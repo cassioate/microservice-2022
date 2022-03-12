b8057725629d9de3b3600bc5cd68dd8d6831c68a
Cassio <cassioate@gmail.com>
2022-03-12 00:44:33 -0300

    zpkin

--- currency-exchange-service/src/main/java/br/com/tessaro/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/br/com/tessaro/controller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package br.com.tessaro.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,8 @@ import br.com.tessaro.service.CurrencyExchangeService;
 @RequestMapping("currency-exchange/")
 public class CurrencyExchangeController {
 	
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+	
 	@Autowired
 	private CurrencyExchangeService currencyService;
 	
@@ -24,6 +28,8 @@ public class CurrencyExchangeController {
 	// allEntries = true) em cima do end point que precisa realziar essa atualização nos caches
 	// fazendo assim com que o cache seja limpado em caso de modificação nesse "produto" que está em cache via outro end point.
 	public ResponseEntity<CurrencyExchange> retrieveExcahngeValue(@PathVariable String from, @PathVariable String to) {
+//		logger.info("adress: "+ "from/"+from+"/to/"+to+"RetrieveExchangevalue call received");
+		logger.info("adress: from/{}/to/{}. - RetrieveExchangevalue call received", from, to);
 		CurrencyExchange currencyExchange = currencyService.retrieveExchangeValue(from, to);
 		return new ResponseEntity<>(currencyExchange, HttpStatus.OK);
 	}
