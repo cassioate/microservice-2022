c936205e9f54f56226856bfc5d8aea462196fa71
Cassio <cassioate@gmail.com>
2022-03-14 22:29:20 -0300

    teste

--- currency-exchange-service/src/main/java/br/com/tessaro/service/CurrencyExchangeService.java
+++ currency-exchange-service/src/main/java/br/com/tessaro/service/CurrencyExchangeService.java
@@ -1,7 +1,5 @@
 package br.com.tessaro.service;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
